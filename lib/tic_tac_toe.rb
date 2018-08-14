efc429e0f0b4ba643b2ecc47919d6849bdea9250
Daniel Schoch <d.schoch90@gmail.com>
2018-08-14 22:23:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,29 +38,30 @@ class TicTacToe
    index.between?(0,8) && !position_taken?(index)
  end
 
- def turn(board)
+ def turn
    puts "Please enter 1-9:"
    input = gets.strip
    index = input_to_index(input)
-   if valid_move?(board, index)
-     move(board, index, current_player(board))
+   if valid_move?(index)
+     move(index, current_player(board))
      display_board(board)
    else
      turn(board)
    end
  end
 
- def turn_count(board)
+ def turn_count
    counter = 0
-   board.each do |player|
+   @board.each do |player|
      if player == "X" || player == "O"
        counter +=1
      end
    end
    return counter
  end
- def current_player(board)
-   turn_count(board).even? == true ? "X" : "O"
+
+ def current_player
+   turn_count.even? == true ? "X" : "O"
  end
 
  def won?(board)
